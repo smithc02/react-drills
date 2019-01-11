d590c5eca8011649f04d2efebf5cd743e2fc63b7
Caleb Smith <calebsupport@Calebs-MacBook-Air.local>
2019-01-11 17:28:58 -0600

    App-5 answer

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Image from './Image';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image myImg={'https://www.jpl.nasa.gov/spaceimages/images/wallpaper/PIA17563-1920x1200.jpg'}/>
       </div>
     );
   }
---
+++ app-5/src/Image.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+
+export default function (props){
+    return(
+        <div>
+            <img src={props.myImg} alt="Space"/>
+        </div>
+    )
+    
+}
